597bd361a74174c7590faa2f9a472056535e2200
Argenis De La Rosa <oftherose91@gmail.com>
2019-07-09 13:19:49 -0400

    updates

---
+++ toLeetSpeak.swift
@@ -0,0 +1,34 @@
+func toLeetSpeak(_ s : String) -> String {
+  
+  let leetSpeak : [Character : Character] = [
+    "A" : "@",
+    "B" : "8",
+    "C" : "(",
+    "D" : "D",
+    "E" : "3",
+    "F" : "F",
+    "G" : "6",
+    "H" : "#",
+    "I" : "!",
+    "J" : "J",
+    "K" : "K",
+    "L" : "1",
+    "M" : "M",
+    "N" : "N",
+    "O" : "0",
+    "P" : "P",
+    "Q" : "Q",
+    "R" : "R",
+    "S" : "$",
+    "T" : "7",
+    "U" : "U",
+    "V" : "V",
+    "W" : "W",
+    "X" : "X",
+    "Y" : "Y",
+    "Z" : "2",
+    " " : " "
+  ]
+  
+  return String(s.characters.flatMap{ leetSpeak[$0] })
+}
