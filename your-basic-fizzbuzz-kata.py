2cb543ff89204542768c7f5c7d5f994f03756344
Argenis De La Rosa <oftherose91@gmail.com>
2019-08-21 18:47:06 -0400

    Create your-basic-fizzbuzz-kata.py

---
+++ your-basic-fizzbuzz-kata.py
@@ -0,0 +1,9 @@
+def fizzbuzz(n):
+    if n % 3 == 0 and n % 5 == 0:
+        return 'fizz buzz'
+    elif n % 3 == 0:
+        return 'fizz'
+    elif n % 5 == 0:
+        return 'buzz'
+    else:
+        return n
